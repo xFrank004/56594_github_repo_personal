b268c7c8dfa609e6c84ac3ff5b2121ac1c23e56e
franrp <frodriguez7@uade.edu.ar>
2026-03-11 21:34:04 -0300

    Mi primer commit

---
+++ repaso.py
@@ -0,0 +1,9 @@
+print("Calculadora Python")
+
+#numero1 = 4
+#numero2 = 6
+numero1 = input("Ingrese el primer numero: ")
+numero2 = input("Ingrese el segundo numero:" )
+suma = numero1 + numero2
+
+print(f"El resultado de la suma es: {suma}")
